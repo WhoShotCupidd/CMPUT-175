7eb5c5b39ddb72bdfcc108116d57566a5f41c2fa
Christiaan <christiaanlemmer@hotmail.com>
2022-12-09 18:57:47 -0700

    quake

---
+++ quakeinversesquare.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
+int main() {
+    float number = 1000;
+    printf("%f", Q_rsqrt(number));
+    return 0;
+}
+
